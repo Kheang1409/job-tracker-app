862e47bc9c4d0d415558f505ec5f8d61804d2e98
Your Name <your-email@example.com>
2025-01-09 17:38:10 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,13 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { EditJobComponent } from './edit-job/edit-job.component';
 import { CreateJobComponent } from './create-job/create-job.component';
+import { JobComponent } from './job/job.component';
 
 
 const home = environment.urlFrontend.home;
 const signIn = environment.urlFrontend.signIn;
 const signUp = environment.urlFrontend.signUp;
+const job_id = environment.urlFrontend.job_id;
 const editJob_id = environment.urlFrontend.editJob_id;
 const createJob = environment.urlFrontend.createJob;
 const error = environment.urlFrontend.error;
@@ -27,6 +29,9 @@ export const routes: Routes = [
     },
     {
         path: signUp, component: SignUpComponent
+    },
+    {
+        path: job_id, component: JobComponent
     },
     {
         path: createJob, component: CreateJobComponent
